1dfef329bd7f58fd61ce7f852ffebd1ece8e1572
NikolayRag <nikolayr@mail.ru>
2017-01-01 01:30:25 +0300

    add form commandline arg list

--- src/__main__.py
+++ src/__main__.py
@@ -1,18 +1,43 @@
 import argparse, sys
 
 from cmdyi import *
+import yi.yiAPI as yiAPI
 from kiLog import *
 
 
 if __name__ == '__main__':
 	cParser= argparse.ArgumentParser(description= 'Yi 4k remote control.')
 
-	cParser.add_argument('-arg', type=bool, nargs='?', const=True, help='')
+	for cmd in yiAPI.registeredCommands:
+		if cmd.names:
+			if cmd.limit:
+				limitZip= list(zip(range(0,len(cmd.limit)), cmd.limit))
+				cParser.add_argument(('-%s' % cmd.commandName),
+					choices=list(str(a) for a,b in limitZip),
+					help=str(dict(limitZip))
+				)
+			else:
+				cParser.add_argument(('-%s' % cmd.commandName))
+		else:
+			cParser.add_argument(('-%s' % cmd.commandName), action='store_true', help='')
+
 	
 	try:
 		args= cParser.parse_args()
 	except:
 		sys.exit(0)
+
+
+	setAny= False
+	for argn,argv in vars(args).items():
+		if argv:
+			setAny= True
+			break
+
+	if not setAny:
+		cParser.print_help()
+		sys.exit(0)
+
 
 
 	kiLog.states(False, ok=False, warn=False)
